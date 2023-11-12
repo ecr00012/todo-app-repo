a31873ee872da0305477623c258b11c4892e1f82
ecr00012 <ecr00012@mix.wvu.edu>
2023-11-12 10:18:14 -0500

    Added disappearing Clear Button that

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import TodoForm from './TodoForm'
 import Todo from './Todo'
 import EditTodoForm from './EditTodoForm'
